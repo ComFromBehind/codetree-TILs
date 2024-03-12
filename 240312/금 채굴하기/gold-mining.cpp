4f2216fb24fac13068d76a3e86f1a4009d8aa0be
DeepSleep <102606875+ComFromBehind@users.noreply.github.com>
2024-03-12 16:15:55 +0900

    Title: 금 채굴하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240312/금 채굴하기/gold-mining.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int n,m;
+int payoff; // 순간 들어가는 비용
+int ans; //금의 개수
+int arr[21][21];
+
+void searching(int k,int a, int b){
+    int temp=0;
+    
+    while(k!=0){
+        int temp = 0;
+        for(int i=-k;i<=k;i++){
+            for(int j=-k;j<=k;j++){
+                if(a+i<0||a+i>=n||b+j<0||b+j>=n) continue;
+                if(i+j<=k&&i+j>=-k&&i-j<=k&&i-j>=-k) temp+=arr[a+i][b+j];
+
+            }
+        }
+        payoff = k*k+(k+1)*(k+1);
+        if(payoff< m*temp){
+            ans = max(ans, temp);
+        }
+        k--;
+    }
+}
+
+int main(){
+    cin>>n>>m;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            searching(n,i,j);
+        }
+    }
+
+    cout<<ans;
+
+
+
+    
+
+}
