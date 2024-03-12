7bd5472e24f7bc8c28f4939819db6d1301f6b514
DeepSleep <102606875+ComFromBehind@users.noreply.github.com>
2024-03-12 18:59:53 +0900

    Title: 기울어진 직사각형, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240312/기울어진 직사각형/slanted-rectangle.cpp
+++ 240312/기울어진 직사각형/slanted-rectangle.cpp
@@ -1,41 +1,49 @@
 #include <iostream>
 using namespace std;
+
 int n;
 int arr[21][21];
-
 int ans;
 
-void func(int k, int a, int b){// k기준으로 왼쪽으로 a칸, 오른쪽으로 b칸
+void func(int r, int c, int left, int right){
+    if(c+right>=n) return;
+    if(c-left<0) return;
+    if(r+left+right>=n) return;
+
     int temp = 0;
-    for(int i=0;i<=a;i++){
-        temp+=arr[i][k-i];
-        temp+=arr[b+i][k+b-i];
+    
+    for(int i=1;i<=left;i++){
+        temp+=arr[r+i][c-i];
+        temp+=arr[r+right+left-i][c-left+right+i];
     }
-    for(int i=1;i<b;i++){
-        temp+=arr[i][k+i];
-        temp+=arr[a+i][k-a+i];
+    for(int i=1;i<=right;i++){
+        temp+=arr[r+i+left][c-left+i];
+        temp+=arr[r+right-i][c+right-i];
     }
+
     ans = max(ans, temp);
 }
 
-int main() {
+int main(){
     cin>>n;
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             cin>>arr[i][j];
-        }
-    }
 
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            for(int k=1;k<n-1;k++){
-                if(k-i<0||k+j>=n) continue;
-                func(k, i,j);
-            }
         }
     }
     
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            for(int k=1;k<n;k++){
+                for(int l=1;l<n;l++){
+                    func(i,j,k,l);
+                }
+            }
+        }
+    }
+
     cout<<ans;
-    // 여기에 코드를 작성해주세요.
-    return 0;
+    
+
 }
