260cd280051ff7b6c8b41d44dadc2f7623444442
DeepSleep <102606875+ComFromBehind@users.noreply.github.com>
2024-03-12 17:20:12 +0900

    Title: 기울어진 직사각형, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240312/기울어진 직사각형/slanted-rectangle.cpp
+++ 240312/기울어진 직사각형/slanted-rectangle.cpp
@@ -5,6 +5,19 @@ int arr[21][21];
 
 int ans;
 
+void func(int k, int a, int b){// k기준으로 왼쪽으로 a칸, 오른쪽으로 b칸
+    int temp = 0;
+    for(int i=0;i<=a;i++){
+        temp+=arr[i][k-i];
+        temp+=arr[b+i][k+b-i];
+    }
+    for(int i=1;i<b;i++){
+        temp+=arr[i][k+i];
+        temp+=arr[a+i][k-a+i];
+    }
+    ans = max(ans, temp);
+}
+
 int main() {
     cin>>n;
     for(int i=0;i<n;i++){
@@ -13,17 +26,13 @@ int main() {
         }
     }
 
-    for(int k=1;k<n-1;k++){
-        //st = arr[0][i];
-        int temp = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if((i-j)==k||(i-j)==-k||(i+j)==k||(i+j)==-k){
-                    temp +=arr[i][j];
-                }
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            for(int k=1;k<n-1;k++){
+                if(k-i<0||k+j>=n) continue;
+                func(k, i,j);
             }
         }
-        ans = max(ans, temp);
     }
     
     cout<<ans;
