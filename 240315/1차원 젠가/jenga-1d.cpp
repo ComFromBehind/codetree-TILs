194523a739876b0871c4398b9b5b2860a8d6b48a
DeepSleep <102606875+ComFromBehind@users.noreply.github.com>
2024-03-15 17:35:57 +0900

    Title: 1차원 젠가, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240315/1차원 젠가/jenga-1d.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int n;
+
+int arr[101];
+int brr[101];
+
+int s1, e1, s2, e2;
+
+int main() {
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    cin>>s1>>e1>>s2>>e2;
+
+    for(int i=0;i<n;i++){
+        if(i>=s1-1&&i<=e1-1) arr[i]=0;
+    }
+
+    for(int i=0;i<n;i++){
+        brr[i]=arr[i];
+    }
+    
+    int count = 0;
+    for(int i=0;i<n;i++){
+        if(arr[i]>0) count++; 
+        if(count>=s2-1&&count<=e2-1) brr[i] = 0;
+    }
+
+    for(int i=0;i<n;i++){
+        arr[i] = brr[i];
+    }
+    
+    count = count - (e2-s2+1);
+    
+    cout<<count<<"\n";
+
+    for(int i=0;i<n;i++){
+        if(arr[i]>0){
+
+            cout<<arr[i]<<"\n";
+        }
+    }
+   
+    return 0;
+}
