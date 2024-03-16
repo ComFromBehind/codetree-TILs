0c6ec6b6c981ea54ab7f2331edb9dbe5fb8ffed3
DeepSleep <102606875+ComFromBehind@users.noreply.github.com>
2024-03-16 22:26:40 +0900

    Title: 십자 모양 폭발, Time: 23ms, Memory: 0MB, Status: Passed - CodeTree

--- 240316/십자 모양 폭발/cross-shape-bomb.cpp
+++ 240316/십자 모양 폭발/cross-shape-bomb.cpp
@@ -4,61 +4,65 @@ using namespace std;
 int n;
 int arr[201][201];
 
-int r,c;
+int r, c;
 
-void bomb(int row, int col){
-    
+void bomb(int row, int col) {
+
     int size = arr[row][col];
 
-    for(int i=0;i<size;i++){
-        if(col-i>=0)
-            arr[row][col-i] = 0;
-        if(col+i<n)
-            arr[row][col+i] = 0;
-        if(row+i<n)
-            arr[row+i][col] = 0;
-        if(row-i>=0)
-            arr[row-i][col] = 0;
+    for (int i = 0;i < size;i++) {
+        if (col - i >= 0)
+            arr[row][col - i] = 0;
+        if (col + i < n)
+            arr[row][col + i] = 0;
+        if (row + i < n)
+            arr[row + i][col] = 0;
+        if (row - i >= 0)
+            arr[row - i][col] = 0;
 
     }
 
-    for(int i=0;i<n;i++){
-        if(i<col-(size-1)||i>col+(size-1)) continue;
-    
-        for(int j=n-1;j>0;j--){
-            int k=j;
-            int temp=0;
-            if(arr[k][i]==0){
-                while(k!=0&&arr[k][i]==0){
-                    if(arr[k-1][i]!=0) {temp = arr[k-1][i]; arr[k-1][i]=0; break;}
+    for (int i = 0;i < n;i++) {
+        if (i<col - (size - 1) || i>col + (size - 1)) continue;
+
+        for (int j = n - 1;j > 0;j--) {
+            int k = j;
+            int temp = 0;
+            
+            if (arr[k][i] == 0) {
+                while (k != 0 && arr[k][i] == 0) {
+                    if (arr[k - 1][i] != 0) { temp = arr[k - 1][i]; arr[k - 1][i] = 0; break; }
                     else k--;
                 }
-                arr[k][i] = temp;
+                arr[j][i] = temp;
+       
             }
+
+            
         }
     }
 
 }
 
-int main(){
-    
-    cin>>n;
-    
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cin>>arr[i][j];
+int main() {
+
+    cin >> n;
+
+    for (int i = 0;i < n;i++) {
+        for (int j = 0;j < n;j++) {
+            cin >> arr[i][j];
         }
     }
 
-    cin>>r>>c;
+    cin >> r >> c;
 
-    bomb(r-1,c-1);
-    
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cout<<arr[i][j]<<" ";
+    bomb(r - 1, c - 1);
+
+    for (int i = 0;i < n;i++) {
+        for (int j = 0;j < n;j++) {
+            cout << arr[i][j] << " ";
         }
-        cout<<"\n";
+        cout << "\n";
     }
 
 
