505ae4ca2ffc8139bcd08a26ab37ae7168213e49
DeepSleep <102606875+ComFromBehind@users.noreply.github.com>
2024-03-16 22:09:20 +0900

    Title: 십자 모양 폭발, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240316/십자 모양 폭발/cross-shape-bomb.cpp
+++ 240316/십자 모양 폭발/cross-shape-bomb.cpp
@@ -23,22 +23,17 @@ void bomb(int row, int col){
     }
 
     for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cout<<arr[i][j]<<" ";
-        }
-        cout<<"\n";
-    }
-
-
-    for(int i=0;i<n;i++){
         if(i<col-(size-1)||i>col+(size-1)) continue;
     
         for(int j=n-1;j>0;j--){
             int k=j;
-            while(k!=0&&arr[k][i]==0){
-                arr[k][i] = arr[k-1][i];
-                if(arr[k-1][i]!=0) arr[k-1][i] = 0;
-                else k--;
+            int temp=0;
+            if(arr[k][i]==0){
+                while(k!=0&&arr[k][i]==0){
+                    if(arr[k-1][i]!=0) {temp = arr[k-1][i]; arr[k-1][i]=0; break;}
+                    else k--;
+                }
+                arr[k][i] = temp;
             }
         }
     }
@@ -59,12 +54,12 @@ int main(){
 
     bomb(r-1,c-1);
     
-    // for(int i=0;i<n;i++){
-    //     for(int j=0;j<n;j++){
-    //         cout<<arr[i][j]<<" ";
-    //     }
-    //     cout<<"\n";
-    // }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
 
 
 }
