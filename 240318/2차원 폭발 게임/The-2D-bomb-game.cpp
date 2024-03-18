c43b955a53503af02b6ff870f552172055135884
DeepSleep <102606875+ComFromBehind@users.noreply.github.com>
2024-03-18 15:58:39 +0900

    Title: 2차원 폭발 게임, Time: 44ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240318/2차원 폭발 게임/The-2D-bomb-game.cpp
+++ 240318/2차원 폭발 게임/The-2D-bomb-game.cpp
@@ -1,130 +1,139 @@
 #include <iostream>
 #include <vector>
+#include <stack>
+#include <utility>
 using namespace std;
 
-int n, m,k;
+int n, m, k;
 
 int arr[101][101];
 int brr[101][101];
 
-
-int bomb(){
-    int flag=0;
-    for(int i=0;i<n;i++){
-        vector<int> v;
-    
-        for(int j=1;j<n;j++){
-            if(arr[j][i]!=arr[j-1][i])
-                v.push_back(j-1);
+void print() {
+    for (int i = 0;i < n;i++) {
+        for (int j = 0;j < n;j++) {
+            cout << arr[i][j] << " ";
         }
-        
-
-        for(int j=0;j<v.size();j++){
-            if(j==0){
-                if(v[j]>=(m-1)){
-                    flag=1;
-                    for(int k=0;k<=v[j];k++){
-                        arr[k][i] = 0;
-                    }
-                }
-            }
-            else{
-                if(v[j]-v[j-1]>=m){
-                    flag=1;
-                    for(int k=v[j-1]+1;k<=v[j];k++){
-                        arr[k][i]=0;
-                    }
-                }
-            }
-            
-        }
-        
+        cout << "\n";
     }
-    return flag;
+    cout << "\n";
 }
 
-void rotate(){ // 시계방향으로 회전.. 
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            brr[i][j] = arr[n-1-j][i];
+void bomb() {
+    for (int i = 0;i < n;i++) {
+
+        vector<pair<int, int>> v;
+
+        v.push_back({ arr[0][i],1 });
+
+        for (int j = 1;j < n;j++) {
+            if (arr[j][i] == arr[j - 1][i]) {
+                v[v.size() - 1].second++;
+
+                if (j == n - 1 && v.back().second >= m)
+                {
+                    
+                    v.pop_back();
+
+                }
+            }
+
+
+
+            else if (arr[j][i] != arr[j - 1][i]) {
+                while (!v.empty()&&v.back().second >= m) {
+                    v.erase(v.end() - 1);
+                }
+                v.push_back({ arr[j][i],1 });
+            }
+        }
+        
+        int st = 0;
+        for (int j = 0;j < v.size();j++) {
+            for (int t = 0;t < v[j].second;t++) {
+                arr[st][i] = v[j].first;
+                st++;
+            }
+        }
+
+        for (int j = st;j < n;j++) {
+            arr[st][i] = 0;
+            st++;
+        }
+    }
+}
+
+
+void rotate() { // 시계방향으로 회전.. 
+    for (int i = 0;i < n;i++) {
+        for (int j = 0;j < n;j++) {
+            brr[i][j] = arr[n - 1 - j][i];
         }
     }
 
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+    for (int i = 0;i < n;i++) {
+        for (int j = 0;j < n;j++) {
             arr[i][j] = brr[i][j];
         }
     }
 
 }
 
-void fall(){
-    for(int i=0;i<n;i++){
+void fall() {
+    for (int i = 0;i < n;i++) {
         vector<int> v;
-        for(int j=n-1;j>=0;j--){
-            if(arr[j][i]!=0)
+        for (int j = n - 1;j >= 0;j--) {
+            if (arr[j][i] != 0)
                 v.push_back(arr[j][i]);
         }
 
         int sz = v.size();
-        for(int j=0;j<sz;j++){
-            arr[n-1-j][i] = v[j];
+        for (int j = 0;j < sz;j++) {
+            arr[n - 1 - j][i] = v[j];
         }
-        for(int j=sz;j<n;j++){
-            arr[n-1-j][i] = 0;
+        for (int j = sz;j < n;j++) {
+            arr[n - 1 - j][i] = 0;
         }
     }
-}
-void print(){
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cout<<arr[i][j]<<" ";
-        }
-        cout<<"\n";
-    }
-    cout<<"\n";
 }
 
-int main(){
-    cin>>n>>m>>k;
-    
-    if(m==1){
-        cout<<0;
+
+int main() {
+    cin >> n >> m >> k;
+
+    if (m == 1) {
+        cout << 0;
         exit(0);
     }
 
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++)
-            cin>>arr[i][j];
+    for (int i = 0;i < n;i++) {
+        for (int j = 0;j < n;j++)
+            cin >> arr[i][j];
     }
 
-    while(k--){
-        
-        
-        int c = bomb();
-        while(c!=0){
-            fall();
-            c = bomb();
-        }
-        
+    while (k--) {
+
+
+        bomb();
         fall();
         rotate();
         fall();
-                 
+
     }
-    
+
     int ans = 0;
-    
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+
+    for (int i = 0;i < n;i++) {
+        for (int j = 0;j < n;j++) {
+
             
-        
-            if(arr[i][j]!=0)
+            if (arr[i][j] != 0)
                 ans++;
         }
        
+
     }
 
 
-    cout<<ans;
+    cout << ans;
 }
