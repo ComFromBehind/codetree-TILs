12f9f51c3dd17602d3b24ec8cd97d825742504f6
DeepSleep <102606875+ComFromBehind@users.noreply.github.com>
2024-03-18 14:32:28 +0900

    Title: 2차원 폭발 게임, Time: 10ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240318/2차원 폭발 게임/The-2D-bomb-game.cpp
+++ 240318/2차원 폭발 게임/The-2D-bomb-game.cpp
@@ -89,13 +89,7 @@ int main(){
         rotate();
         fall();
         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                cout<<arr[i][j]<<" ";
-            }
-            cout<<"\n";
-        }
-        cout<<"\n\n"
+    
     }
     
     int ans = 0;
