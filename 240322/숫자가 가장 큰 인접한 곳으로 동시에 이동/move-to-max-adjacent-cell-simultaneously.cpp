04a863334759fbbe58d2198923d8f8dce549c765
DeepSleep <102606875+ComFromBehind@users.noreply.github.com>
2024-03-22 23:32:33 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240322/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+
+int n,m,t;
+int arr[21][21];
+int brr[21][21];
+int r,c;
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, -1, 1};
+
+void reset(){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++)
+            brr[i][j] = 0;
+    }
+}
+
+void move(int row,int col){
+    int max_idx = 0;
+    int max_val = 0;
+    
+    for(int i=0;i<4;i++){
+        int nx = dx[i]+row;
+        int ny = dy[i]+col;
+        if(nx<0||nx>=n||ny<0||ny>=n) continue;
+        if(max_val<arr[nx][ny]){
+            max_idx = i;
+            max_val = arr[nx][ny];
+        }
+    }
+
+    brr[row+dx[max_idx]][col+dy[max_idx]]++;
+}
+
+void crash_check(){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){           
+            if(brr[i][j]>1) brr[i][j] = 0;
+        }
+    }
+}
+
+void copy(){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            arr[i][j] = brr[i][j];
+        }
+    }
+}
+
+int main(){
+    cin>>n>>m>>t;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++)
+            cin>>arr[i][j];
+    }
+    
+    while(m--){
+        cin>>r>>c;
+        
+        arr[r-1][c-1] = 1;
+    }
+    while(t--){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(arr[i][j]!=0){
+                    move(i,j);
+
+                }
+            }
+        }
+        crash_check();
+        copy();
+        reset();
+    }
+
+    int ans = 0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(arr[i][j]==1) ans++; 
+        }
+    }
+    cout<<ans;
+}
