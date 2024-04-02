87443b9015919b2f82654b73231fd18befe0ea31
DeepSleep <102606875+ComFromBehind@users.noreply.github.com>
2024-04-02 16:44:56 +0900

    Title: 2명의 도둑, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240402/2명의 도둑/two-thieves.cpp
+++ 240402/2명의 도둑/two-thieves.cpp
@@ -6,53 +6,79 @@ using namespace std;
 int n,m,c;
 int arr[11][11];
 int ans;
-
+int smallans;
 bool check[11][11];
-
+vector<int> tmp;
 vector<pair<int,int>> v;
 
 bool checking(int i, int j){
-    int temp = arr[i][j];
-    if(j<n-1&&temp+arr[i][j+1]<=c){
-        if(check[i][j]!=0||check[i][j+1]!=0) return false;
-        check[i][j]=1;
-        check[i][j+1]=1;
+    for(int t = j; t<j+m; t++){
+        if(check[i][t]!=0) return false;
     }
-    else{
-        if(check[i][j]!=0) return false;
-        check[i][j] = 1;
+    for(int t = j;t<j+m;t++){
+        check[i][t] = 1; 
     }
     return true;
 }
 
 bool unchecking(int i, int j){
-    int temp = arr[i][j];
-    if(j<n-1&&temp+arr[i][j+1]<=c){
-        if(check[i][j]!=1||check[i][j+1]!=1) return false;
-        check[i][j]=0;
-        check[i][j+1] = 0;
+    for(int t = j; t<j+m; t++){
+        if(check[i][t]!=1) return false;
     }
-    else{
-        if(check[i][j]!=1) return false;
-        check[i][j] = 0;
+    for(int t = j;t<j+m;t++){
+        check[i][t] = 0; 
     }
     return true;
 }
 
+vector<pair<int,int>> w;
+
+void func2(int k){
+
+    if(k==m){
+        int sum=0;
+        for(int i=0;i<tmp.size();i++){
+            sum+=arr[w[tmp[i]].first][w[tmp[i]].second];
+        }
+        if(sum>c) {smallans = 0; return;}
+
+        int temp_weight = 0;
+        for(int i=0;i<tmp.size();i++){
+            temp_weight += arr[w[tmp[i]].first][w[tmp[i]].second]*arr[w[tmp[i]].first][w[tmp[i]].second];
+        }
+        smallans = max(smallans, temp_weight);
+        return;
+    }
+    
+    tmp.push_back(k);
+    func2(k+1);
+    tmp.pop_back();
+    func2(k+1);
+    
+}
+
 void calc(){
     int temp_w=0;
+
     for(int i=0;i<2;i++){
-        int st = arr[v[i].first][v[i].second];
-        int st1 = st*st;
-        if(v[i].second<n-1&&arr[v[i].first][v[i].second+1]+st<=c){
-            int st2 =  arr[v[i].first][v[i].second+1]*arr[v[i].first][v[i].second+1];
-            temp_w+=st2;
+        w.clear();
+        
+        int row = v[i].first;
+        int col = v[i].second;
+        
+        for(int j=0;j<m;j++){
+            w.push_back({row,col+j});
         }
-        temp_w+=st1;
+        tmp.clear();
+        smallans=0;
+        func2(0);
+        temp_w += smallans;
     }
     
     ans = max(temp_w, ans);
 }
+
+
 
 void func(int k){
     if(k==2){
@@ -62,7 +88,7 @@ void func(int k){
     }
 
     for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+        for(int j=0;j<n-m+1;j++){
             if(checking(i,j)){
                 v.push_back({i,j});
                 func(k+1);
