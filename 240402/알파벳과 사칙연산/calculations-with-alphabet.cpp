ba3412ea8bf1cbb7afac9e2ed8c89dc7d30566bf
DeepSleep <102606875+ComFromBehind@users.noreply.github.com>
2024-04-02 19:50:40 +0900

    Title: 알파벳과 사칙연산, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240402/알파벳과 사칙연산/calculations-with-alphabet.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int n;
+string s;
+
+char list[6] = {'a','b','c','d','e','f'};
+int check[6] = {-1,-1,-1,-1,-1,-1}; 
+
+vector<int> alpha;
+int m;
+
+int ans;
+int arr[6]; //숫자 매핑
+
+vector<int> mapping;
+
+void calc(){
+    int cnt = 0;
+    
+    char op2 = ' ';
+    for(int i=0;i<s.length();i++){
+        if(s[i]>='a'&&s[i]<='f'){
+            if(cnt==0){
+                cnt = arr[s[i]-'a'];
+            }
+            else{
+                if(op2=='+')
+                    cnt+=arr[s[i]-'a'];
+                else if(op2=='-')
+                    cnt-=arr[s[i]-'a'];
+                else if(op2=='*')
+                    cnt*=arr[s[i]-'a'];
+            }
+        }
+        else if(s[i]=='+'){
+            op2 = '+';
+        }
+        else if(s[i]=='-'){
+            op2 = '-';
+        }
+        else if(s[i]=='*'){
+            op2 = '*';
+        }
+    }
+    ans = max(cnt, ans);
+}
+
+void func(int k ){
+    if(k==m){
+        
+        for(int i=0;i<mapping.size();i++){
+            arr[alpha[i]] = mapping[i]; 
+        }
+        calc();
+
+        return;
+    }
+    for(int i=1;i<=4;i++){
+        mapping.push_back(i);
+        func(k+1);
+        mapping.pop_back();
+    }
+}
+ //짝수번째에 무조건 연산자가 오게 되어있다!
+
+int main(){
+    cin>>s;
+    
+    for(int i=0; i<s.length();i++ ){
+        if(s[i] >= 'a' && s[i]<='f') {
+            if(check[s[i]-'a']==-1){
+                check[s[i]-'a']=0;
+                alpha.push_back(s[i]-'a'); //alpha에는 스트링에 들어잇는 문자대표 1개씩 들어잇음.
+        
+            }
+        }
+    }
+    m = alpha.size();
+    func(0);
+
+    cout<<ans;
+
+}
