501389de064d126c71c0887dfebf71134ee50cd9
DeepSleep <102606875+ComFromBehind@users.noreply.github.com>
2024-04-11 16:12:34 +0900

    Title: 그래프 탐색, Time: 14ms, Memory: 0MB, Status: Passed - CodeTree

--- 240411/그래프 탐색/graph-traversal.cpp
+++ 240411/그래프 탐색/graph-traversal.cpp
@@ -5,16 +5,16 @@ using namespace std;
 int ans;
 int n,m;
 
-int arr[1001][1001];
 bool check[1001];
 
+vector<int> node[1001];
 
 void dfs(int k){
-    for(int i=1;i<=n;i++){
-        if(arr[k][i]==1&&!check[i]){
+    for(int i=0;i<node[k].size();i++){
+        if(!check[node[k][i]]){
             ans++;
-            check[i] = 1;
-            dfs(i);
+            check[node[k][i]] = 1;
+            dfs(node[k][i]);
         }
     }
 }
@@ -24,11 +24,10 @@ int main(){
     for(int i=0; i<m;i++ ){
         int tmp1, tmp2;
         cin>>tmp1>>tmp2;
-
-        arr[tmp1][tmp2] = 1;
-        arr[tmp2][tmp1] = 1;      
+        node[tmp1].push_back(tmp2);
+        node[tmp2].push_back(tmp1);
     }
-    check[1]  =1;
+    check[1] =1;
     dfs(1);
     
     cout<<ans;
