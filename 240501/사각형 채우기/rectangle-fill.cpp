74cfd1d452c2d11987d8d037c289946ee5f1ad67
DeepSleep <102606875+ComFromBehind@users.noreply.github.com>
2024-05-01 17:54:40 +0900

    Title: 사각형 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240501/사각형 채우기/rectangle-fill.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int n;
+int arr[1001];
+
+int main(){
+
+    cin>>n;
+    arr[1] = 1;
+    arr[2] = 2;
+
+    for(int i=3;i<=n;i++){
+        arr[i] = (arr[i-1]+arr[i-2])%10007;
+    }
+    cout<<arr[n];
+
+}
