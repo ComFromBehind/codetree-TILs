f5f686b4224c06006da3fd6e891afb01efdcf6b7
DeepSleep <102606875+ComFromBehind@users.noreply.github.com>
2024-05-01 15:42:59 +0900

    Title: 피보나치 수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240501/피보나치 수/fibonacci-number.cpp
+++ 240501/피보나치 수/fibonacci-number.cpp
@@ -7,15 +7,25 @@ using namespace std;
 int n;
 int arr[46];
 
+int fibo(int m){
+    if(arr[m]!=-1)
+        return arr[m];
+    if(m<=2) return 1;
+    else{
+        arr[m] = fibo(m-1)+fibo(m-2);
+    }
+    return arr[m]; 
+}
+
 int main(){
     
     cin>>n;
     
-    arr[1] = 1;
-    arr[2] = 1;
-    for(int i=3;i<=n;i++){
-        arr[i] = arr[i-1]+arr[i-2];
+    for(int i=0;i<=n;i++){
+        arr[i] = -1;
     }
-    cout<<arr[n];
+
+    cout<<fibo(n);
+
 
 }
