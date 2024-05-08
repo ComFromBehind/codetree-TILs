61ddefef9d6a597899c514c9211d7554ff275608
DeepSleep <102606875+ComFromBehind@users.noreply.github.com>
2024-05-08 10:57:25 +0900

    Title: 사각형 채우기 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240508/사각형 채우기 3/rectangle-fill-3.cpp
+++ 240508/사각형 채우기 3/rectangle-fill-3.cpp
@@ -18,10 +18,10 @@ int main(){
     for(int i=4;i<=n;i++){
         int sum = 0;
         for(int j=i-3;j>=0;j--){
-            sum+=2*(arr[j]);
+            sum+=(2*(arr[j]))%1000000007;
         }
-        sum+=2*arr[i-1];
-        sum+=3*arr[i-2];
+        sum+=(2*arr[i-1])%1000000007;
+        sum+=(3*arr[i-2])%1000000007;
         arr[i] = sum%1000000007;
     }
     cout<< arr[n];  
