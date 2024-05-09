04be53f0c43e4a0f041333a9871292302571bd5c
DeepSleep <102606875+ComFromBehind@users.noreply.github.com>
2024-05-09 16:51:19 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 14ms, Memory: 4MB, Status: Passed - CodeTree

--- 240509/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.cpp
+++ 240509/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.cpp
@@ -1,46 +1,71 @@
 #include <iostream>
-#include <vector>
-#include <utility>
+#include <algorithm>
+
 using namespace std;
 
 int n;
-
+int dp[101][101][101];
 int arr[101][101];
-pair<int,int> dp[101][101];
 
-int main() {
-    
+void initialize(){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            for(int k=1;k<=100;k++){
+                dp[i][j][k] = 1e9;
+            }
+        }
+    }
+    dp[0][0][arr[0][0]] = arr[0][0];
+
+    for(int i=1;i<n;i++){
+        for(int k=1;k<=100;k++){
+            dp[i][0][min(k,arr[i][0])] = min(
+                dp[i][0][min(k, arr[i][0])],
+                max(dp[i-1][0][k], arr[i][0])
+            );
+            
+        }
+    }
+    for(int i=1;i<n;i++){
+        for(int k=1;k<=100;k++){
+            dp[0][i][min(k,arr[0][i])]= min(
+                dp[0][i][min(k,arr[0][i])],
+                max(dp[0][i-1][k], arr[0][i])
+            );
+        }
+    }
+}
+
+void solve(){
+    initialize();
+
+    for(int i=1;i<n;i++){
+        for(int j=1;j<n;j++){
+            for(int k=1;k<=100;k++){
+                dp[i][j][min(k,arr[i][j])] = min(
+                    dp[i][j][min(k, arr[i][j])],
+                    max(min(dp[i-1][j][k],dp[i][j-1][k]), arr[i][j])
+                );
+            }
+        }
+    }
+}
+
+
+int main(){
     cin>>n;
-    
+
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             cin>>arr[i][j];
         }
     }
+    solve();
 
-    dp[0][0] = {arr[0][0],arr[0][0]};
-    
-    for(int i=1;i<n;i++){
-        dp[0][i] = {max(dp[0][i-1].first,arr[0][i]),min(dp[0][i-1].second,arr[0][i])};
-        dp[i][0] = {max(dp[i-1][0].first,arr[i][0]),min(dp[i-1][0].second,arr[i][0])};
-
+    int ans = 1e9;
+    for(int k=1;k<=100;k++){
+        if(dp[n-1][n-1][k]!=1e9)
+            ans = min(ans, dp[n-1][n-1][k]-k);
     }
-
-    for(int i=1;i<n;i++){
-        for(int j=1;j<n;j++){
-            pair<int,int> hubo1 = {max(dp[i-1][j].first,arr[i][j]), min(dp[i-1][j].second, arr[i][j])};
-            int hubo11 = hubo1.first-hubo1.second;
-
-            pair<int,int> hubo2 = {max(dp[i][j-1].first, arr[i][j]),min(dp[i][j-1].second,arr[i][j])};
-            int hubo22 = hubo2.first- hubo2.second;
-
-            if(abs(hubo11)>=abs(hubo22)) dp[i][j] = hubo2;
-            else dp[i][j] = hubo1;
-           
-        }
-
-    }
-   
-    cout<<dp[n-1][n-1].first-dp[n-1][n-1].second;
-    return 0;
+    cout<<ans;
 }
