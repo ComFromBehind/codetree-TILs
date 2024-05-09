f2b272c76577df15d1fbb9b9dd812ffdeaa816da
DeepSleep <102606875+ComFromBehind@users.noreply.github.com>
2024-05-09 17:29:16 +0900

    Title: 최대 증가 부분 수열, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240509/최대 증가 부분 수열/longest-increasing-subsequence.cpp
+++ 240509/최대 증가 부분 수열/longest-increasing-subsequence.cpp
@@ -10,17 +10,17 @@ int dp[1001];
 int main(){
     cin>>n;
     
-    for(int i=0;i<n;i++){
+    for(int i=1;i<=n;i++){
         cin>>arr[i];
     }
 
-    dp[0] = 1;
+    dp[0] = 0;
 
     for(int i=1;i<n;i++){
         dp[i] = -1e8;
     }
 
-    for(int i=1;i<n;i++){
+    for(int i=1;i<=n;i++){
         for(int j=0;j<i;j++){
             if(arr[j]<arr[i]){
                 dp[i] = max(dp[j]+1, dp[i]);
