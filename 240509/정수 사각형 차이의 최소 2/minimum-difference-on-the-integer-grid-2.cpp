4f81557c329841fb7d5483a1ad9c7112c2dc4740
DeepSleep <102606875+ComFromBehind@users.noreply.github.com>
2024-05-09 15:03:45 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240509/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.cpp
+++ 240509/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.cpp
@@ -22,18 +22,25 @@ int main() {
     
     for(int i=1;i<n;i++){
         dp[0][i] = {max(dp[0][i-1].first,arr[0][i]),min(dp[0][i-1].second,arr[0][i])};
-        dp[i][0] = {max(dp[i-1][0].first,arr[i-1][0]),min(dp[i-1][0].second,arr[i-1][0])};
+        dp[i][0] = {max(dp[i-1][0].first,arr[i][0]),min(dp[i-1][0].second,arr[i][0])};
 
     }
 
     for(int i=1;i<n;i++){
         for(int j=1;j<n;j++){
-            dp[i][j] = {max(min(dp[i-1][j].first,dp[i][j-1].first),arr[i][j]),min(max(dp[i-1][j].second,dp[i][j-1].second),arr[i][j])};
-        
+            pair<int,int> hubo1 = {max(dp[i-1][j].first,arr[i][j]), min(dp[i-1][j].second, arr[i][j])};
+            int hubo11 = hubo1.first-hubo1.second;
+
+            pair<int,int> hubo2 = {max(dp[i][j-1].first, arr[i][j]),min(dp[i][j-1].second,arr[i][j])};
+            int hubo22 = hubo2.first- hubo2.second;
+
+            if(abs(hubo11)>=abs(hubo22)) dp[i][j] = hubo2;
+            else dp[i][j] = hubo1;
+           
         }
 
     }
-    
+   
     cout<<dp[n-1][n-1].first-dp[n-1][n-1].second;
     return 0;
 }
